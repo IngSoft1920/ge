3ef0b1fe66769bce8d8594296da4c4939abd1727
Jorge Vazquez <jorge.vazquez.acevedo@alumnos.upm.es>
2020-01-31 15:42:55 +0100

    Incluir Logger + Test ejemplo

--- src/main/java/ingsoft1920/ge/GeApplication.java
+++ src/main/java/ingsoft1920/ge/GeApplication.java
@@ -1,12 +1,16 @@
 package ingsoft1920.ge;
 
+import org.apache.logging.log4j.LogManager;
+import org.apache.logging.log4j.Logger;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
 public class GeApplication {
-
+	final static Logger logger = LogManager.getLogger(GeApplication.class.getName());
+	
 	public static void main(String[] args) {
+		logger.warn("Aplicacion iniciada");
 		SpringApplication.run(GeApplication.class, args);
 	}
 

a3396e04098bda85842b5717ae459085fc22fe63
Alfonso <alfonso.jimenez.galan@alumnos.upm.es>
2020-02-18 19:54:56 +0100

    Borrado imports de Json

--- src/main/java/ingsoft1920/ge/Controller/ge_Controller.java
+++ src/main/java/ingsoft1920/ge/Controller/ge_Controller.java
@@ -13,8 +13,8 @@ import org.springframework.web.bind.annotation.PostMapping;
 
 import ingsoft1920.ge.Beans.SesionBean;
 import ingsoft1920.ge.HttpClient.HttpClient;
-import ingsoft1920.ge.Model.UsuarioModel;
-import net.minidev.json.JSONUtil;
+
+
 
 
 @Controller

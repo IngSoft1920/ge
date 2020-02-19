c5d4b764f05f9cb4d67036d53b95adfac297b055
Ivan Toro Otero <ivan.toro.otero@alumnos.upm.es>
2020-02-19 12:59:01 +0100

    cambio misreservas controller

--- src/main/java/ingsoft1920/ge/Controller/MisReservasController.java
+++ src/main/java/ingsoft1920/ge/Controller/MisReservasController.java
@@ -13,33 +13,18 @@ import org.springframework.web.bind.annotation.PostMapping;
 import ingsoft1920.ge.Beans.MisReservasBean;
 import ingsoft1920.ge.Beans.ReservaBean;
 
-
 @Controller
 public class MisReservasController {
 	final static Logger logger = LogManager.getLogger(MisReservasController.class.getName());
 
-	@GetMapping("/reservas")
+	@GetMapping("/misReservas")
 	public String buscarGet(Model model) {
-		
-		// Consulta a la base de datos
-		MisReservasBean misReservasPendientes = new MisReservasBean();
-		misReservasPendientes.getReservas().add(new ReservaBean("Hotel 1", "Zaragoza", "28/4/2020", "30/4/2020", "Suit", "300.0", ""));
-		
-		MisReservasBean misReservasPasadas = new MisReservasBean();
-		misReservasPasadas.getReservas().add(new ReservaBean("Hotel 2", "Madrid", "12/1/2020", "14/1/2020", "Suit", "300.0", ""));
-		
-		
-		model.addAttribute("misReservasPendientes", misReservasPendientes);
-		model.addAttribute("misReservasPasadas", misReservasPasadas);
-		model.addAttribute("mensajeError","");
-		
-		return "reserva";
-	}
-	
-	@PostMapping("/reservar")
-	public String buscarPost(@Valid @ModelAttribute("misReservasBean") MisReservasBean misReservasBean,
-			Model model) {
-		
-		return "reserva";
+
+		MisReservasBean misReservasBean = new MisReservasBean();
+		model.addAttribute("misReservasBean", misReservasBean);
+		model.addAttribute("mensajeError", "");
+
+		return "misReservas";
+
 	}
 }

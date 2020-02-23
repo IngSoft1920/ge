ee8b12448d40668fc7f41ae64ef34165ff724e52
Isaac <isaac.lacortm@alumnos.upm.es>
2020-02-23 20:59:50 +0100

    Conexión de Consulta de disponibilidad lista

--- src/main/java/ingsoft1920/ge/Beans/BusquedaBean.java
+++ src/main/java/ingsoft1920/ge/Beans/BusquedaBean.java
@@ -3,7 +3,9 @@ package ingsoft1920.ge.Beans;
 
 
 import java.util.ArrayList;
+import java.util.HashMap;
 import java.util.List;
+import java.util.Map;
 
 import org.springframework.stereotype.Component;
 
@@ -13,38 +15,23 @@ public class BusquedaBean {
 	private String fechaFin;
 	private String hotel;
 	private String ciudad;
+	private int hotel_id;
 	private List<String> hoteles;
+	private Map<String, Integer> ids;
 	private List<String> ciudades;
 	
 	public BusquedaBean() {
 		this.hoteles = new ArrayList<String>();
+		this.ids = new HashMap<String, Integer>();
 		this.ciudades = new ArrayList<String>();
 	}
 	
 	public boolean checkCamposValidos() {
 		String now = java.time.LocalDate.now().toString();
-		String[] fecha = now.split("-"); // fecha = {"<año>", "<mes>", "<dia>"}
-		String[] fechaInicio = this.fechaInicio.split("-");
-		String[] fechaFin = this.fechaFin.split("-");
-		if (fechaInicio.length != 3 || fechaFin.length != 3 ||
-			fechaInicio[0].compareTo(fecha[0]) < 0 || 
-			fechaFin[0].compareTo(fechaInicio[0]) < 0 ||
-			fechaInicio[1].compareTo(fecha[1]) < 0 ||
-			fechaFin[1].compareTo(fechaInicio[1]) < 0 ||
-			fechaInicio[2].compareTo(fecha[2]) < 0 ||
-			fechaFin[2].compareTo(fechaInicio[2]) <= 0) {
-			
-			return false;
-		}
-		return true;
-	}
-	
-	public List<String> getHoteles() {
-		return hoteles;
-	}
-
-	public void setHoteles(List<String> hoteles) {
-		this.hoteles = hoteles;
+		if (now.compareTo(this.fechaInicio) <= 0 && 
+				this.fechaInicio.compareTo(this.fechaFin) < 0)
+			return true;
+		return false;
 	}
 
 	public List<String> getCiudades() {
@@ -87,5 +74,61 @@ public class BusquedaBean {
 		this.ciudad = ciudad;
 	}
 
+	public int getHotel_id() {
+		return hotel_id;
+	}
+
+	public void setHotel_id(int hotel_id) {
+		this.hotel_id = hotel_id;
+	}
+
+	public List<String> getHoteles() {
+		return hoteles;
+	}
+
+	public void setHoteles(List<String> hoteles) {
+		this.hoteles = hoteles;
+	}
+
+	public Map<String, Integer> getIds() {
+		return ids;
+	}
+
+	public void setIds(Map<String, Integer> ids) {
+		this.ids = ids;
+	}
+	
+	public int getNumeroDias () {
+		String[] inS = fechaInicio.split("-");
+		String[] outS = fechaFin.split("-");
+		int[] in = new int[] 
+				{Integer.parseInt(inS[0]), Integer.parseInt(inS[1]), Integer.parseInt(inS[2])};
+		int[] out = new int[] 
+				{Integer.parseInt(outS[0]), Integer.parseInt(outS[1]), Integer.parseInt(outS[2])};
+		int dias = 0;
+		
+		while (in[0] != out[0] || in[1] != out[1]) {
+
+			switch (in[1]) {
+			case 1: case 3: case 5: case 7: case 8: case 10:
+				dias += 31;
+				in[1] += 1;
+				break;
+			case 4: case 6: case 9: case 11:
+				dias += 30;
+				in[1] += 1;
+				break;
+			case 12:
+				dias += 31;
+				in[0] += 1;
+				in[1] = 1;
+				break;
+			default:
+				dias += (in[0] % 4 == 0) ? 29 : 28;
+				in[1] += 1;
+			}
+		}
+		return dias + (out[2]-in[2]);
+	}
 	
 }

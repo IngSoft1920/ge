26f78c151ba2633b521fe5a11e253186968cfca3
Gonzalo <gonzaloom99@gmail.com>
2020-03-13 17:25:44 +0100

    Servicios Reservados
    Coger datos de dho de servicios reservados por un cliente

--- src/main/java/ingsoft1920/ge/ControllerGE1/ServiciosController.java
+++ src/main/java/ingsoft1920/ge/ControllerGE1/ServiciosController.java
@@ -23,62 +23,83 @@ import ingsoft1920.ge.HttpClient.HttpClient;
 
 @Controller
 public class ServiciosController {
-	
+
 	final static Logger logger = LogManager.getLogger(ServiciosController.class.getName());
-	
+
 
 	@Autowired
 	ServiciosBean servicios;
+	@Autowired
 	SesionBean sesion;
 	VerReservasBean reservas;
-	
-	
+
 	@GetMapping("/servicios")
 	public String checkInEnviar(Model model, SesionBean sesion) throws Exception {
-        model.addAttribute("sesionBean", sesion);
+		model.addAttribute("sesionBean", sesion);
 		return "servicios";
- 	}
-	
-	
+	}
+
+
 
 	//recibir servicios
-		@GetMapping("/recibirServicios")
-		public static  String serviciosEnviar() throws Exception {
-			
-			HttpClient client= new HttpClient("http://piedrafita.ls.fi.upm.es:7001/serviciosDisponibles", "POST");
-			
-			//enviar nombre del hotel
-			JsonObject json = new JsonObject();
-			json.addProperty("nombre_hotel", "hotel_prueba");//habria que cogerlo de VerReservasBean, ¿como?
-			client.setRequestBody(json.toString());
-			
-			int respCode = client.getResponseCode();
-			
-			String resp="";
-			if(respCode==200) {
-				  resp=client.getResponseBody();
-				  }
-			return resp;
-			
-			
+	@GetMapping("/recibirServicios")
+	public static  String recibirServiciosr(@ModelAttribute("reserva") VerReservasBean reserva) throws Exception {
+
+		HttpClient client= new HttpClient("http://piedrafita.ls.fi.upm.es:7001/serviciosDisponibles", "POST");
+
+		//enviar nombre del hotel
+		JsonObject json = new JsonObject();
+		json.addProperty("nombre_hotel", "hotel_prueba");//habria que cogerlo de VerReservasBean, ¿como?
+		//ASI?? json.addProperty("id_estancia", reserva.getNombre_hotel());   ASI¿?
+		client.setRequestBody(json.toString());
+
+		int respCode = client.getResponseCode();
+
+		String resp="";
+		if(respCode==200) {
+			resp=client.getResponseBody();
 		}
+		return resp;
+	}
 
 	public  String recibirServicios(@Valid @ModelAttribute("serviciosBean") ServiciosBean servicios,
 			Model model) throws Exception {
-		
+
 		HttpClient client= new HttpClient("piedrafita.ls.fi.upm.es:7001/serviciosDisponibles", "POST");
-		
+
 		return "";
 	}
-		
-		//falta api que nos de las horas disponibles y mandamos fecha e id de servicio
-		
-		
-		
+
+	//falta api que nos de las horas disponibles y mandamos fecha e id de servicio
+
+
+
+	//recibir servicios reservados por el cliente
+	@GetMapping("/serviciosReservados")
+	public static  String serviciosReservados(@Valid @ModelAttribute("sesion") SesionBean sesion,
+			@ModelAttribute("reserva") VerReservasBean reserva) throws Exception {
+
+		HttpClient client= new HttpClient("http://piedrafita.ls.fi.upm.es:7001/serviciosReservados", "POST");
+
+		//enviar id_cliente e id_reserva
+		JsonObject json = new JsonObject();
+		json.addProperty("id_cliente", sesion.getUsuarioID()); //coger id_usuario de la sesionBean
+		json.addProperty("id_estancia", reserva.getId_reserva()); //coger id_reserva de VerReservasBean
+		client.setRequestBody(json.toString());
+
+		int respCode = client.getResponseCode();
+
+		String resp="";
+		if(respCode==200) {
+			resp=client.getResponseBody();
+		}
+		return resp;
+	}
+
 	//reservar servicios
 	@PostMapping("/reservarServicios1")	
-	public String serviciosReservados(@Valid @ModelAttribute("serviciosBean") ServiciosBean servicos) throws Exception{
-			
+	public String reservarServicios(@Valid @ModelAttribute("serviciosBean") ServiciosBean servicos) throws Exception{
+
 		HttpClient client= new HttpClient("http://piedrafita.ls.fi.upm.es:7001/reservar_servicio", "POST");
 		JsonObject json = new JsonObject();
 		json.addProperty("tipoServicio", servicios.getTipoServicio());//se puede mandar el servicio en vez del id??
@@ -86,14 +107,14 @@ public class ServiciosController {
 		json.addProperty("hora", servicios.getHoras());
 		json.addProperty("cliente_id", sesion.getUsuarioID());
 		//falta el lugar,¿como?
-		
+
 		//cosas que faltan
 		json.addProperty("numPersonas", servicios.getNumPersonas());
 		json.addProperty("idReserva", reservas.getId_reserva());
 		json.addProperty("platos", (String)null);
 		json.addProperty("items", (String)null);
 		//falta el nombre del restaurante al reservar una mesa
-		
+
 		client.setRequestBody(json.toString());
 		int respCode = client.getResponseCode();
 		System.out.println(respCode+"\n");
@@ -101,13 +122,13 @@ public class ServiciosController {
 		{
 			client.getResponseBody();
 		}
-		
-		
+
+
 		return "";
 	}
-	
-	
-	
+
+
+
 	public static Object beanToJson(Object bean) {
 		Gson gson = new Gson();
 		String JSON = gson.toJson(bean);	

1f5215c59d8ef16e915dbc41c744b631ed9a5eca
Gonzalo <gonzaloom99@gmail.com>
2020-04-26 13:13:00 +0200

    Completar check in y comprobar fecha valida

--- src/main/java/ingsoft1920/ge/ControllerGE1/VerReservasController.java
+++ src/main/java/ingsoft1920/ge/ControllerGE1/VerReservasController.java
@@ -27,109 +27,120 @@ import ingsoft1920.ge.HttpClient.HttpClient;
 
 @Controller
 public class VerReservasController {
-public static JSONObject receivedJSON = new JSONObject();
-public static Reserva reservilla;
-	
-@Autowired
- SesionBean sesion;
+	public static JSONObject receivedJSON = new JSONObject();
+	public static Reserva reservilla;
+
+	@Autowired
+	SesionBean sesion;
 
 
-@GetMapping("/recibirReservas")
+	@GetMapping("/recibirReservas")
 	public  ModelAndView reservasEnviar() throws Exception {
-		
+
 		receivedJSON.put("datosReserva", "Datos de su reserva");
 
 		HttpClient client= new HttpClient("http://piedrafita.ls.fi.upm.es:7001/reservas","POST");
 		JsonObject json= new JsonObject();
 		json.addProperty("id_cliente", datosController.ALFONSO);
 		client.setRequestBody(json.toString());
-		
+
 		int respCode = client.getResponseCode();
-		
+
 		String resp="";
 		if(respCode==200) {
-			  resp=client.getResponseBody();
-			  }
-		
+			resp=client.getResponseBody();
+		}
+
 		JsonObject obj = (JsonObject) JsonParser.parseString(resp);
 		//numero de reserva
 		JsonArray numeros_reservas= obj.get("id_estancia_lista").getAsJsonArray();
-		
+
 		//numero de habitacion
 		JsonArray numeros_habitaciones= obj.get("num_hab_lista").getAsJsonArray();
-		
-		
+
+
 		//fecha inicio
 		JsonArray inicio_fechas= obj.get("fecha_Inicio_Lista").getAsJsonArray();
-		
-		
+
+
 		//fecha final
 		JsonArray final_fechas= obj.get("fecha_Fin_Lista").getAsJsonArray();
-		
-		
+
+
 		//nombre hotel
 		JsonArray nombre_hoteles= obj.get("nombre_hotel_Lista").getAsJsonArray();
 		JsonArray estado= obj.get("estado").getAsJsonArray();
 		List<Reserva> reservas= new LinkedList<>();
 		for (int i=0;i<nombre_hoteles.size();i++) {
-			reservas.add(new Reserva(numeros_reservas.get(i).getAsInt(),numeros_habitaciones.get(i).getAsInt(),inicio_fechas.get(i).getAsString(),final_fechas.get(i).getAsString(),nombre_hoteles.get(i).getAsString(),estado.get(i).getAsString()));
+			reservas.add(new Reserva(numeros_reservas.get(i).getAsInt(),
+					numeros_habitaciones.get(i).getAsInt(),inicio_fechas.get(i).getAsString(),
+					final_fechas.get(i).getAsString(),nombre_hoteles.get(i).getAsString(),
+					estado.get(i).getAsString()));
 		}
-		
-		
+
+
 		return new ModelAndView("reservaServicios","reservas", reservas);
-		
+
 	}
 
 	@PostMapping("/gestionar/{id}")
 	public String enviarComanda(@PathVariable("id") int id) throws Exception {
-		
+
 		HttpClient client= new HttpClient("http://piedrafita.ls.fi.upm.es:7001/reservas","POST");
 		JsonObject json= new JsonObject();
 		json.addProperty("id_cliente", datosController.ALFONSO);
-		
+
 		client.setRequestBody(json.toString());
-		
+
 		int respCode = client.getResponseCode();
-		
+
 		String resp="";
 		if(respCode==200) {
-			  resp=client.getResponseBody();
-			  }
-		
+			resp=client.getResponseBody();
+		}
+
 		JsonObject obj = (JsonObject) JsonParser.parseString(resp);
 		//numero de reserva
 		JsonArray numeros_reservas= obj.get("id_estancia_lista").getAsJsonArray();
-		
+
 		//numero de habitacion
 		JsonArray numeros_habitaciones= obj.get("num_hab_lista").getAsJsonArray();
-		
-		
+
+
 		//fecha inicio
 		JsonArray inicio_fechas= obj.get("fecha_Inicio_Lista").getAsJsonArray();
-		
-		
+
+
 		//fecha final
 		JsonArray final_fechas= obj.get("fecha_Fin_Lista").getAsJsonArray();
-		
-		
+
+
 		//nombre hotel
 		JsonArray nombre_hoteles= obj.get("nombre_hotel_Lista").getAsJsonArray();
 		//estado hotel
 		JsonArray estado= obj.get("estado").getAsJsonArray();
-		
+
 		int cont=0;
 		boolean salir= false;
 		for(int i=0;i<estado.size();i++) {
 			if (numeros_reservas.get(i).getAsInt()==id && salir==false) {
 				cont=i;salir=true;
 			}
-			
+
 		}
 		Reserva res= new Reserva(numeros_reservas.get(cont).getAsInt(),numeros_habitaciones.get(cont).getAsInt(),inicio_fechas.get(cont).getAsString(),final_fechas.get(cont).getAsString(),nombre_hoteles.get(cont).getAsString(),estado.get(cont).getAsString());
 		reservilla=res;
 		System.out.print(res.toString());
 		return "index";
-			
+
 	}
-	
+
+	//rellenar datos que faltan y enviar( el envio a falta de que dho pueda recibir nuevos datos)
+	@PostMapping("/completarCheckin")
+	public String completar() {
+
+		return "completarCheckin";
+	}
+
+
 }
